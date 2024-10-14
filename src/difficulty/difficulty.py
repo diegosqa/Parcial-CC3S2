c50f2ee90c62252d0ed0b66641594e585d71375f
diegosqa <101370172+diegosqa@users.noreply.github.com>
2024-10-14 15:22:42 -0500

    Update difficulty.py

--- src/difficulty/difficulty.py
+++ src/difficulty/difficulty.py
@@ -1,10 +1,6 @@
 class Difficulty:
-    """Clase para manejar los niveles de dificultad."""
-
     def __init__(self, nivel):
         self.nivel = nivel
-
-    # Obtener el archivo de palabras correspondiente al nivel de dificultad
     def obtener_archivo_por_dificultad(self):
         if self.nivel == 'fácil':
             return 'words_easy.txt'
